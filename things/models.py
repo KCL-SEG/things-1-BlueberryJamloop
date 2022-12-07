9b9a1ff530f8c77f6de23e00169f1aa20aad3c76
Maja G <maja293@outlook.com>
2022-12-07 12:38:47 +0000

    Migrations

--- things/models.py
+++ things/models.py
@@ -5,5 +5,5 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
+        validators=[MinValueValidator(0), MaxValueValidator(100)], unique=False
     )
